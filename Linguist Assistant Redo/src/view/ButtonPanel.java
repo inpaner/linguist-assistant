2dfd331b13df8fe155c78927b2ce8db54a5db258
Ivan Paner <ivan.paner@gmail.com>
2013-10-15 19:14:28 +0800

    Added scrollPane to ButtonPanel

---
+++ Linguist Assistant Redo/src/view/ButtonPanel.java
@@ -0,0 +1,32 @@
+package view;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import model.Constituent;
+import net.miginfocom.swing.MigLayout;
+
+@SuppressWarnings("serial")
+public class ButtonPanel extends JPanel {
+	//List<Constituent> defaultCons;
+    private final int ROW_WIDTH = 5;
+
+    public ButtonPanel() {
+        // listeners = new ArrayList<>();
+        setLayout(new MigLayout());
+        JPanel innerPanel = new JPanel();
+        innerPanel.setLayout(new MigLayout("wrap " + ROW_WIDTH, "fill"));
+        for(Constituent c : Constituent.getAllConstituents()) {
+                innerPanel.add(new DraggableButton(c));
+            
+        }
+        JScrollPane scrollPane = new JScrollPane(innerPanel);
+        add(scrollPane);
+    }
+    public static void main(String[] args) {
+        System.out.println("Started");
+        MainFrame frame = new MainFrame();
+        ButtonPanel panel = new ButtonPanel();
+        frame.setPanel(panel);
+    }
+}
