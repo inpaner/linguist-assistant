4d8d65b01d51cd987f2b9f3efa300ec31788c17f
laronandrew11 <laronandrew11@yahoo.com>
2013-10-21 12:57:08 +0800

    Added lexicon parser classes

---
+++ Linguist Assistant Redo/src/ontology/model/POS.java
@@ -0,0 +1,36 @@
+package ontology.model;
+
+import grammar.model.Feature;
+
+public class POS {
+	
+	private String label;
+	//private Entry[] entry;
+	private Feature[] feature;
+	private Feature[] forms;
+	
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	/*public Entry[] getEntry() {
+		return entry;
+	}
+	public void setEntry(Entry[] entry) {
+		this.entry = entry;
+	}*/
+	public Feature[] getFeature() {
+		return feature;
+	}
+	public void setFeature(Feature[] feature) {
+		this.feature = feature;
+	}
+	public Feature[] getForms() {
+		return forms;
+	}
+	public void setForms(Feature[] forms) {
+		this.forms = forms;
+	}
+}
