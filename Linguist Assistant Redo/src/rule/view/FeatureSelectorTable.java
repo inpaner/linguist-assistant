40bb7e1553be84164489d263572967feabb96802
Ivan Paner <ivan.paner@gmail.com>
2013-11-10 15:41:51 +0800

    Made use of a Map for setting selected values

--- Linguist Assistant Redo/src/rule/view/FeatureSelectorTable.java
+++ Linguist Assistant Redo/src/rule/view/FeatureSelectorTable.java
@@ -1,21 +1,23 @@
 package rule.view;
 
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import grammar.model.Category;
 import grammar.model.Feature;
 
 import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.table.AbstractTableModel;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.TableCellEditor;
-import javax.swing.table.TableCellRenderer;
+import javax.swing.JSeparator;
 
 import commons.main.MainFrame;
 import net.miginfocom.swing.MigLayout;
@@ -24,102 +26,115 @@ import net.miginfocom.swing.MigLayout;
  * http://www.coderanch.com/t/343795/GUI/java/Check-Box-JTable-header
  */
 public class FeatureSelectorTable extends JPanel {
-    public static void main(String[] args) {
-        
+    private List<Feature> features;
+    private List<List<FeatureCheckbox>> checkboxesList = new ArrayList<>();
+    private Map<Feature, FeatureCheckbox> checkboxMap = new HashMap<>();
+    private JButton ok;
+    private JButton cancel;
+    
+    public static void main(String[] args) {    
         MainFrame frame = new MainFrame();
         Category category = Category.getByName("Noun");
         FeatureSelectorTable panel = new FeatureSelectorTable(category);
         frame.setPanel(panel);
+        
+        Feature feature = Feature.getEmpty(category);
+        feature.setName("number");
+        feature.setValue("dual");
+        
+        List<Feature> test = new ArrayList<>();
+        test.add(feature);
+        
+        panel.setSelected(test);
     }
     
-    private List<List<TableCellRenderer>> editorsList = new ArrayList<>();
-    private JTable table;
-    private List<Feature> features;
     public FeatureSelectorTable(Category category) {
-        features = category.getFeatures();
         
-        setLayout(new MigLayout());
-
+        /*
+         * Checkbox inner panel
+         */
+        features = category.getFeatures();
+        JPanel innerPanel = new JPanel();
+        innerPanel.setLayout(new MigLayout("flowy"));
         
         for (Feature feature : features) {
-            List<TableCellRenderer> editors = new ArrayList<>();
+            JLabel featureLabel = new JLabel(feature.getName());
+            List<FeatureCheckbox> checkboxes = new ArrayList<>();
+            innerPanel.add(featureLabel);
+            innerPanel.add(new JSeparator(), "growx");
+            
             for (String value : feature.getPossibleValues()) {
-                Feature empty = Feature.getEmpty(category);
-                empty.setValue(value);
-                final FeatureCheckbox checkbox = new FeatureCheckbox(feature);
-                System.out.println(checkbox.getValue());
-                
-                TableCellRenderer cellEditor = new TableCellRenderer() {
-                    @Override
-                    public Component getTableCellRendererComponent(JTable table, Object value,
-                            boolean isSelected, boolean hasFocus, int row, int column) {
-                        return checkbox;
-                    }
-                };
-                editors.add(cellEditor);
-            }
-            editorsList.add(editors);
-        }
-        
-        
-        table = new JTable() {
-            @Override
-            public TableCellRenderer getCellRenderer(int row, int column) {
-                List<TableCellRenderer> editors = editorsList.get(column);
-                if (row < editors.size()) {
-                    return editors.get(row);
-                }
-                else {
-                    return super.getCellRenderer(row, column);
-                }
-            }
-        };
-        
-        table.setModel(new FeatureTableModel());
-        JScrollPane scrollPane = new JScrollPane(table);
-        add(scrollPane);
-    }
 
-    private class FeatureTableModel extends AbstractTableModel {
-        public FeatureTableModel() {}
+                Feature copy = Feature.copy(feature);
+                copy.setValue(value);
+                FeatureCheckbox checkbox = new FeatureCheckbox(copy); 
+                innerPanel.add(checkbox);
+                checkboxes.add(checkbox);
+                checkboxMap.put(copy, checkbox);
+            }
+            checkboxesList.add(checkboxes);
+            innerPanel.add(new JLabel(), "wrap, gap 70");
+        }   
+        JScrollPane scrollpane = new JScrollPane(innerPanel);
+
+        /*
+         * Buttons
+         */
+        ok = new JButton("Ok");
+        cancel = new JButton("Cancel");
+        ok.addActionListener(new OkListener());
+        cancel.addActionListener(new CancelListener());
+        
+        setLayout(new MigLayout());
+        add(scrollpane, "wrap");
+        add(ok, "span, split, right");
+        add(cancel);
+    }
     
-        public int getColumnCount() {
-            return features.size();
-        }
-    
-        public int getRowCount() {
-            int max = 0;
-            for (Feature feature : features) {
-                int size = feature.getPossibleValues().size();
-                if (size > max) {
-                    max = size;
+    public List<List<Feature>> getSelected() {
+        List<List<Feature>> result = new ArrayList<>();
+        
+        for (List<FeatureCheckbox> checkboxes : checkboxesList) {
+            List<Feature> features = new ArrayList<>();
+            for (FeatureCheckbox checkbox : checkboxes) {
+                if (checkbox.isSelected()) {
+                    System.out.println(checkbox.getFeature() + " " + checkbox.getValue());
+                    features.add(checkbox.getFeature());
                 }
             }
-            
-            return max;
-        }
-        
-        public String getColumnName(int columnIndex) {
-            
-            return features.get(columnIndex).getName();
-        }
-    
-        public Object getValueAt(int rowIndex, int columnIndex) {
-            Feature feature = features.get(rowIndex);
-            Object value;
-            switch (columnIndex) {
-                case 0: value = feature.getName();
-                        break;
-                case 1: value = feature.getValue();
-                        break;
-                default: value = "";
+            if (!features.isEmpty()) {
+                result.add(features);
             }
-            
-            return value;
         }
         
-        public boolean isCellEditable(int rowIndex, int columnIndex) {
-            return (columnIndex == 1);
+        return result;
+    }
+    
+    
+    public void setSelected(List<Feature> features) {
+        for (Feature basis : features) {
+            FeatureCheckbox checkbox = checkboxMap.get(basis);
+            if (checkbox != null) {
+                checkbox.setSelected(true);
+            }
         }
+    }
+    
+    private class OkListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent ev) {
+            getSelected();
+        }
+        
+    }
+    
+    private class CancelListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent ev) {
+            
+        }
+        
     }
 }
