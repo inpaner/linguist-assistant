05aaa519a76db3f23d17b0552fdc767378aef67c
Ivan Paner <ivan.paner@gmail.com>
2013-11-16 17:00:09 +0800

    Added names to Spellout Enums

--- Linguist Assistant Redo/src/rule/spellout/ModType.java
+++ Linguist Assistant Redo/src/rule/spellout/ModType.java
@@ -1,7 +1,34 @@
 package rule.spellout;
 
 public enum ModType {
-    PREFIX, SUFFIX, INFIX, CIRUMFIX, NEW_TRANSLATION, ADD_WORD;
+    PREFIX("Prefix"), 
+    SUFFIX("Suffix"), 
+    INFIX("Infix"), 
+    CIRUMFIX("Circumfix"), 
+    NEW_TRANSLATION("New Translation"), 
+    ADD_WORD("Add Word");
     
+    public static ModType get(String type) {
+        switch(type.toUpperCase()) {
+            case "PREFIX": return PREFIX;
+            case "SUFFIX": return SUFFIX;
+            case "INFIX": return INFIX;
+            case "CIRCUMFIX": return CIRUMFIX;
+            case "NEW TRANSLATION":
+            case "NEW_TRANSLATION": return NEW_TRANSLATION;
+            case "ADD WORD":
+            case "ADD_WORD": return ADD_WORD;
+            default: return null;
+        }
+    }
     
+    private final String name;
+    ModType(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public String toString() {
+        return name;
+    }
 }
--- Linguist Assistant Redo/src/rule/spellout/SpelloutType.java
+++ Linguist Assistant Redo/src/rule/spellout/SpelloutType.java
@@ -1,16 +1,30 @@
 package rule.spellout;
 
 public enum SpelloutType {
-    SIMPLE, TABLE, MORPHOPHONEMIC, FORM, PHRASE;
+    SIMPLE("Simple"), 
+    TABLE("Table"), 
+    MORPHOPHONEMIC("Morphophonemic"), 
+    FORM("Form"), 
+    PHRASE("Phrase");
     
     public static SpelloutType get(String type) {
-        switch(type.toLowerCase()) {
-            case "simple": return SIMPLE;
-            case "table": return TABLE;
-            case "morphophonemic": return MORPHOPHONEMIC;
-            case "form": return FORM;
-            case "phrase": return PHRASE;
+        switch(type.toUpperCase()) {
+            case "SIMPLE": return SIMPLE;
+            case "TABLE": return TABLE;
+            case "MORPHOPHONEMIC": return MORPHOPHONEMIC;
+            case "FORM": return FORM;
+            case "PHRASE": return PHRASE;
             default: return null;
         }
+    }
+    
+    private final String name;
+    SpelloutType(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public String toString() {
+        return name;
     }
 }
