ef5906197057a1c16b96075d3de74cfc9674daa4
mikeypdl <mikeypdl@yahoo.com>
2013-11-17 18:32:25 +0800

    removed duplicate methods in Input

--- Linguist Assistant Redo/src/rule/model/input/Input.java
+++ Linguist Assistant Redo/src/rule/model/input/Input.java
@@ -24,10 +24,4 @@ public abstract class Input {
     public void setRoot(Rule root) {
         this.root = root;
     }
-    public boolean isOptional() {
-        return optional;
-    }
-    public void setOptional(boolean optional) {
-        this.optional = optional;
-    }
 }
